a7c99e5d53412d009c536732290798edb40a01a7
azahel79 <azahelprogramer79@gmail.com>
2022-12-30 15:27:54 -0600

    3:27 pm

--- config/db/connectDB.js
+++ config/db/connectDB.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 exports.connectDB = async()=>{    
                   
   try {     
+    mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.URI);
     console.log("conectado en la base de datos de mongoDB");
   } catch (error) {
